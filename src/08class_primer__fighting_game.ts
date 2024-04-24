b6c7262299cea1c17f1fbb905f17d99ca085a39a
d-ito1106 <d_ito@taiho-pp.co.jp>
2024-04-24 14:10:29 +0900

    complete 08

--- src/08class_primer__fighting_game.ts
+++ src/08class_primer__fighting_game.ts
@@ -1,13 +1,19 @@
-const testinput = `3 6
-10 1 1 2 2 3 3
-10 0 0 6 1 7 2
-10 0 0 7 5 8 3
+const testinput = `5 10
+8 2 24 40 25 42 26
+59 48 13 21 13 56 2
+5 59 7 57 5 25 24
+99 28 6 32 5 23 2
+62 24 19 11 19 7 21
+2 1 3 2
+2 1 3 2
+5 1 3 1
+5 3 1 2
 1 1 2 2
-1 2 3 2
-1 3 2 3
-2 2 3 1
-2 3 3 1
-1 2 3 2`
+4 2 3 1
+5 3 3 2
+2 3 3 2
+4 1 5 3
+2 3 3 2`
 class Input {
   public inputDataArray: string[] = []
   public topItem = ''
@@ -41,17 +47,16 @@ class Player {
   public name: number
   public hp: number
   public skill: Skill[]
+  public isAlive: boolean
   constructor(hp: number, Skill: Skill[], name: number) {
     this.hp = hp
     this.skill = Skill
     this.name = name
+    this.isAlive = true
   }
   //プレイヤーは攻撃メソッドとバフメソッドをもつ
   action(skill_num: number): Skill {
     const myskill: Skill = this.skill[skill_num - 1]
-    if (myskill.isBuff) {
-      this.buff()
-    }
     return myskill
   }
   buff() {
@@ -65,8 +70,10 @@ class Player {
     })
   }
   damage(damage: number) {
-    if (this.hp <= 0) return
     this.hp -= damage
+    if (this.hp <= 0) {
+      this.isAlive = false
+    }
   }
 }
 //技クラス
@@ -74,11 +81,14 @@ class Skill {
   public power: number
   public flame: number
   public isBuff: boolean
-  constructor(power: number, flame: number) {
+  public isAttack: boolean
+  constructor(flame: number, power: number) {
     if (power === 0 && flame === 0) {
       this.isBuff = true
+      this.isAttack = false
     } else {
       this.isBuff = false
+      this.isAttack = true
     }
     this.power = power
     this.flame = flame
@@ -112,21 +122,34 @@ function main(inputStr: string) {
     const player = new Player(hp, skillArray, i + 1)
     playerArray.push(player)
   }
-  // console.log(playerArray)
 
   //プレイヤーの攻撃
   for (let i = times[0]; i < times[0] + times[1]; i++) {
     const attackker = playerArray[(inputorderArray[i][0] as number) - 1]
     const attackerSkill = attackker.action(inputorderArray[i][1] as number)
     const defense = playerArray[(inputorderArray[i][2] as number) - 1]
-    const diffenceSkill = defense.action(inputorderArray[i][3] as number)
+    const defenceSkill = defense.action(inputorderArray[i][3] as number)
 
-    if (attackerSkill !== null && diffenceSkill !== null) {
-      if (attackerSkill.flame > diffenceSkill.flame) {
+    if (!attackker.isAlive || !defense.isAlive) {
+      continue
+    }
+    if (attackerSkill.isAttack && defenceSkill.isAttack) {
+      if (attackerSkill.flame < defenceSkill.flame) {
         defense.damage(attackerSkill.power)
-      } else {
-        attackker.damage(diffenceSkill.power)
+      } else if (attackerSkill.flame > defenceSkill.flame) {
+        attackker.damage(defenceSkill.power)
       }
+    } else if (attackerSkill.isBuff && defenceSkill.isBuff) {
+      attackker.buff()
+      defense.buff()
+    } else if (attackerSkill.isBuff && defenceSkill.isAttack) {
+      attackker.buff()
+      attackker.damage(defenceSkill.power)
+    } else if (attackerSkill.isAttack && defenceSkill.isBuff) {
+      defense.buff()
+      defense.damage(attackerSkill.power)
+    } else {
+      throw new Error('error')
     }
   }
 
@@ -138,5 +161,4 @@ function main(inputStr: string) {
   }
   console.log(count)
 }
-
 main(testinput)
