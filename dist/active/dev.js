39d9cc2c84b70ebfb0a5f4e025e1f0cab80d27d7
d-ito1106 <d_ito@taiho-pp.co.jp>
2024-06-13 17:39:37 +0900

    uphoge

--- dist/active/dev.js
+++ dist/active/dev.js
@@ -1,78 +1,85 @@
-'use strict'
+"use strict";
 const testinput = `10
-3 11 18 25 40 58 69 81 88 99
+45 62 11 81 75 90 13 2 97 32
 5
-11
-100
-2
-41
-69`
+90
+75
+50
+1
+63`;
 class Input {
-  constructor(str, splitby = '\n') {
-    this.inputDataArray = []
-    this.topItem = ''
-    const inputArr = str.split(splitby)
-    this.inputDataArray = inputArr.filter(Boolean)
-  }
-  shiftTop(stringOrReg = ' ') {
-    const shifted = this.inputDataArray.shift()
-    const splited =
-      shifted === null || shifted === void 0
-        ? void 0
-        : shifted.split(stringOrReg)
-    if (typeof splited === 'undefined') {
-      return []
-    } else {
-      return this.stringToNum(splited)
+    constructor(str, splitby = '\n') {
+        this.inputDataArray = [];
+        this.topItem = '';
+        const inputArr = str.split(splitby);
+        this.inputDataArray = inputArr.filter(Boolean);
     }
-  }
-  stringToNum(stringArr) {
-    return stringArr.map((e) => {
-      if (e.match(/^-?[0-9]+$/)) {
-        return Number(e)
-      } else {
-        return e
-      }
-    })
-  }
+    shiftTop(stringOrReg = ' ') {
+        const shifted = this.inputDataArray.shift();
+        const splited = shifted === null || shifted === void 0 ? void 0 : shifted.split(stringOrReg);
+        if (typeof splited === 'undefined') {
+            return [];
+        }
+        else {
+            return this.stringToNum(splited);
+        }
+    }
+    stringToNum(stringArr) {
+        return stringArr.map((e) => {
+            if (e.match(/^-?[0-9]+$/)) {
+                return Number(e);
+            }
+            else {
+                return e;
+            }
+        });
+    }
 }
 function main(inputStr) {
-  const input = new Input(inputStr)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const _times = input.shiftTop()
-  const inputorderArray = []
-  input.inputDataArray.forEach((e) => {
-    const formatted = input.stringToNum(new Input(e, ' ').inputDataArray)
-    inputorderArray.push(formatted)
-  })
-  // const times = inputorderArray[2]
-  const searchDataBase = inputorderArray.shift()
-  const searchTargetLength = inputorderArray.shift()
-  //number[]として扱う為にアサーションをする
-  const searchTarget = inputorderArray
-  const search = binarySearch(searchDataBase, 1)
-  searchTarget.forEach((element) => {
-    if (binarySearch(searchDataBase, element[0])) {
-      console.log('Yes')
-    } else {
-      console.log('No')
-    }
-  })
+    const input = new Input(inputStr);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const _times = input.shiftTop();
+    const inputorderArray = [];
+    input.inputDataArray.forEach((e) => {
+        const formatted = input.stringToNum(new Input(e, ' ').inputDataArray);
+        inputorderArray.push(formatted);
+    });
+    // const times = inputorderArray[2]
+    const sortTargetArray = inputorderArray.shift();
+    const targetAllay = inputorderArray.shift();
+    sortTargetArray.sort((a, b) => a - b);
+    // console.log(sortTargetArray)
+    inputorderArray.forEach((element) => {
+        const searched = binary_search(sortTargetArray, sortTargetArray.length, element[0]);
+        // console.log('element', element[0])
+        // console.log('searched', searched)
+        const count = sortTargetArray.length - searched;
+        if (count < 0) {
+            console.log(0);
+        }
+        else {
+            console.log(count);
+        }
+    });
 }
-function binarySearch(arr, target) {
-  let left = 0
-  let right = arr.length - 1
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2)
-    if (arr[mid] === target) {
-      return true
-    } else if (arr[mid] < target) {
-      left = mid + 1
-    } else if (target < arr[mid]) {
-      right = mid - 1
+const binary_search = (A, n, k) => {
+    //(A : 数列, n : 数列のサイズ, k : 基準)
+    // 探索範囲 [left, right]
+    let left = 0;
+    let right = n;
+    // 探索範囲を狭めていく
+    while (left < right) {
+        // 探索範囲の中央
+        const mid = Math.floor((left + right) / 2); //切り下げ
+        if (A[mid] <= k) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
     }
-  }
-  return false
-}
+    // 狭め終わったらmin_iを返す
+    return right;
+};
 // main(testinput);
 main(require('fs').readFileSync('/dev/stdin', 'utf8'))
