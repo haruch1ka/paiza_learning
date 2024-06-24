283df1636d828dbdf8da2ad6a246906587a779b2
d-ito1106 <d_ito@taiho-pp.co.jp>
2024-06-24 11:54:39 +0900

    add 07

--- dist/active/dev.js
+++ dist/active/dev.js
@@ -9,30 +9,42 @@ function main(inputStr) {
     // console.log(L, n, k)
     let a = lines[1].split(' ').map(Number);
     a = [0, ...a, L];
-    console.log(a);
-    let left = 0, right = L + 1;
+    let [left, right] = [0, L];
+    // console.log('L', L, 'n', n, 'k', k)
+    // console.log(a)
+    // console.log('left', left, 'right', right)
+    for (let i = 1; i < k + 2; i++) {
+        left = Math.max(left, a[i] - a[i - 1]);
+    }
+    left -= 1;
+    // console.log('left', left)
     while (right - left > 1) {
         const mid = Math.floor((left + right) / 2);
-        let last_index = 0, parts = 0;
-        for (let i = 0; i < k + 2; i++) {
-            console.log('mid', mid, 'a[i]', a[i], 'a[last_index]', a[last_index], 'i', i);
-            console.log('diff ', a[i] - a[last_index]);
-            if (a[i] - a[last_index] >= mid) {
-                parts += 1;
-                last_index = i;
-                console.log('-----------------');
-                console.log('lastindex ', last_index);
-                console.log('-----------------');
+        let last_index = 0, index = 0, parts = 0;
+        const flag = true;
+        while (flag) {
+            // console.log('---------------------------------')
+            // console.log('index + 1', index + 1, 'k + 2', k + 2)
+            while (index + 1 < k + 2 && a[index + 1] - a[last_index] <= mid) {
+                index += 1;
+                // console.log('diff', a[index] - a[last_index])
             }
+            // console.log('---------------------------------')
+            // console.log('diff', a[index] - a[last_index], 'index', index)
+            parts += 1;
+            if (index == k + 1) {
+                break;
+            }
+            last_index = index;
         }
-        if (parts < n) {
-            right = mid;
-        }
-        else {
+        if (parts > n) {
             left = mid;
         }
+        else {
+            right = mid;
+        }
     }
-    console.log(left);
+    console.log(right);
 }
 main(testinput);
 // // main(require('fs').readFileSync('/dev/stdin', 'utf8').trim())
